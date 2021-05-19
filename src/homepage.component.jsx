027e29cf01f3d47c92f7cc394d06f32ac603a8e9
xleed-dev <xleed@protonmail.ch>
2021-05-18 20:10:46 -0500

    Created initial styles. SCSS installed

---
+++ src/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import './homepage.styles.scss'
+
+const HomePage = () => (
+  <div className='homepage'>
+    <div className="directory-menu">
+      <div className="menu-item">
+        Fuck
+      </div>
+    </div>
+  </div>
+)
+
+export default HomePage;
