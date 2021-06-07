15e2fc292d8a6b98f30ea27ea7f457969d67fad6
xCast <xleed@protonmail.ch>
2021-06-07 12:48:54 -0500

    created nodejs backend for payments, react project moved to /client

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 
 import StripeCheckout from "react-stripe-checkout";
+import axios from 'axios'
 
 const StripeChecloutButton = ({price}) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51Iw8CJI9B0cl8tAzS3WbhQ3qiK5NkTGwakQwTRP0xp8JxIASZU8NAlBTrhoqOHrPebA1BYHzHrwdBvGdVhCUWzbs00jAsGCvGJ'
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment successful!')
+    axios({
+      url: '/payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    })
+    .then(response => {
+      alert('Payment successful!')
+    })
+    .catch(error => {
+      console.log('Payment error:', error);
+      alert('There was an issue with you payment')
+    })
   }
 
   return(
